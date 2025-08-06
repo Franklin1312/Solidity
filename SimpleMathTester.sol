03504e16260a22415e9c8f45485b998cf96621ed
Franklin1312 <franklinbabu333@gmail.com>
2025-08-06 23:26:09 +0530

    Create SimpleMathTester.sol

---
+++ SimpleMathTester.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SafeMathTester{
+    uint8 public bignumber=255;
+
+    function add() public {
+        unchecked {bignumber=bignumber+1;}
+    }
+}
